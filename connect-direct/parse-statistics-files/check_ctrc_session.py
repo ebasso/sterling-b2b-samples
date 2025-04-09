d3613bfc1751b96d49603c7bd6283ef6c8549b3c
Enio Basso <ebasso@ebasso.net>
2025-04-09 09:57:34 -0300

    Add files via upload

---
+++ connect-direct/parse-statistics-files/check_ctrc_session.py
@@ -0,0 +1,45 @@
+import os
+import re
+
+def process_file(file_path, pnum_dict):
+    with open(file_path, 'r') as file:
+        for line in file:
+            if "PNUM=" in line:
+                # Extract PNUM
+                pnum_start = line.find("PNUM=") + len("PNUM=")
+                pnum_end = line.find("|", pnum_start)
+                pnum = line[pnum_start:pnum_end]
+
+                star = line[5:26]
+
+                # Create a dict for PNUM, if not exist
+                if pnum not in pnum_dict:
+                    pnum_dict[pnum] = {"RECI_SSTR": False, "RECI_CTRC": False, "RECI_SEND": False, "STAR": star}
+
+                # define and update RECID
+                if "SSTR" in line:
+                    pnum_dict[pnum]["RECI_SSTR"] = True
+                if "CTRC" in line:
+                    pnum_dict[pnum]["RECI_CTRC"] = True
+                if "SEND" in line:
+                    pnum_dict[pnum]["RECI_SEND"] = True
+                
+
+def process_directory(directory_path):
+    pnum_dict = {}
+    pattern = re.compile(r"S\d{8}\.\d{3}")
+
+    for filename in os.listdir(directory_path):
+        if pattern.match(filename):
+            file_path = os.path.join(directory_path, filename)
+            process_file(file_path, pnum_dict)
+
+
+    # Print PNUMs and flags
+    for pnum, flags in pnum_dict.items():
+       if not pnum_dict[pnum]["RECI_CTRC"]:
+           print(f"PNUM={pnum}, RECI_SSTR={flags['RECI_SSTR']}, RECI_CTRC={flags['RECI_CTRC']}, RECI_SEND={flags['RECI_SEND']}, STAR={flags['STAR']}")
+
+# Process in current path:
+directory_path = '.'
+process_directory(directory_path)
